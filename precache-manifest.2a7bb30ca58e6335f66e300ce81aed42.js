1e0bd67801b07c3111050fbe1cc6a7ca6ce6b313
Julie <juliebell14@gmail.com>
2020-01-24 20:26:06 -0600

    Updates

---
+++ precache-manifest.2a7bb30ca58e6335f66e300ce81aed42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48881ec818f17fc14f0db82e730cb81",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "2eb9b2038c4488c51bf4",
+    "url": "/meetup/static/css/main.029a4330.chunk.css"
+  },
+  {
+    "revision": "0704a3d8cefcf8645e54",
+    "url": "/meetup/static/js/2.cafd8b82.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/meetup/static/js/2.cafd8b82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2eb9b2038c4488c51bf4",
+    "url": "/meetup/static/js/main.56d10859.chunk.js"
+  },
+  {
+    "revision": "a280c9bdd30b51d60b29",
+    "url": "/meetup/static/js/runtime-main.a2b58e33.js"
+  }
+]);
