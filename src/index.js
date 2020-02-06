19ac86629445447af065013093b7ea61dd5310a3
Julie <juliebell14@gmail.com>
2020-02-05 21:26:57 -0600

    installed atatus

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +11,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+atatus.config('d7b63578f1a44f81b3abb982dc8d42f5').install();
