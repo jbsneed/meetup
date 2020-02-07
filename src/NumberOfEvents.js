247fac2d4f1350be008579dffe11eb474f23c5eb
Julie <juliebell14@gmail.com>
2020-02-06 22:41:00 -0600

    adding alert messages

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import { ErrorAlert } from './Alert';
+
+
 class NumberOfEvents extends Component {
     state = {
         query: 32, //default number of events shown
@@ -7,12 +10,26 @@ class NumberOfEvents extends Component {
     handleInputChanged = (event) => {
         const value = event.target.value;
         this.setState({ query: value });
-        this.props.updateEvents(null, null, value);
+
+        if (value <= 0) {
+            this.setState({
+                infoText: 'You gotta search for at least one thing!'
+            });
+        } else {
+            this.setState({
+                infoText: ''
+            })
+
+            this.props.updateEvents(null, null, value);
+        }
     }
 
     render() {
         return (
             <div className="events">
+                <div className="alertMessage">
+                    <ErrorAlert text={this.state.infoText} />
+                </div>
                 <input
                     type="number"
                     className="eventsNumberTextbox"
