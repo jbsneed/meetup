e8a985e5f1d2c37426c31465411d5dbd236e9435
Julie <juliebell14@gmail.com>
2020-02-10 15:30:45 -0600

    pie chart

--- src/Event.js
+++ src/Event.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer, Legend, Tooltip } from 'recharts';
 
 class Event extends Component {
     state = {
@@ -7,26 +8,66 @@ class Event extends Component {
     }
 
     handleChange = () => {
-        this.setState({ showDetails: !this.state.showDetails })
+        this.setState({ showDetails: !this.state.showDetails });
+        const rsvpCount = this.props.event.yes_rsvp_count;
+        const rsvpMax = this.props.event.rsvp_limit;
+        if (rsvpCount === rsvpMax) {
+            this.setState({ infoText: 'This event has 0 vacancies.' })
+        } else {
+            this.setState({ infoText: '' });
+        }
     }
+
+    getData = () => {
+        const taken = this.props.event.yes_rsvp_count;
+        const limit = this.props.event.rsvp_limit;
+        const remaining = limit - taken;
+
+        return (
+            [
+                { "name": "Spots Taken", "value": taken },
+                { "name": "Spots Remaining", "value": remaining }
+            ]
+        )
+    }
+
     render() {
         const event = this.props.event;
+        let colors = ['blue', 'green'];
+        let buttonText = 'More';
+        if (this.state.showDetails) {
+            buttonText = 'Less';
+        }
         return (
             <div className="Event">
                 <div className="eventName">{event.name}</div>
                 <div className="eventDate">{event.local_date}</div>
-
-                <button className="detailsButton" onClick={this.handleChange}>Show More</button>
-
                 {this.state.showDetails && (
                     <div className="eventDetails">
+                        {event.rsvp_limit &&
+                            <ResponsiveContainer width="99%" height="99%">
+                                <PieChart width={300} height={200}>
+                                    <Legend />
+                                    <Pie data={this.getData()} fill="#8884d8" label>
+                                        {
+                                            this.getData().map((entry, index) => (
+                                                <Cell key={`cell-${index}`} fill={colors[index]} />
+                                            ))
+                                        }
+                                    </Pie>
+                                    <Tooltip />
+                                </PieChart>
+                            </ResponsiveContainer>
+                        }
                         <div className="eventCity">City: {event.venue && event.venue.city}</div>
                         <div className="eventCity">State: {event.venue && event.venue.state}</div>
                         <div className="eventAddress">Address: {event.venue && event.venue.address_1}</div>
                         <div className="eventDescription" dangerouslySetInnerHTML={{ __html: event.description }}></div>
-                        <button className="lessDetailsButton" onClick={this.handleChange}>Show Less</button>
+                        <div>  <a className="eventLink" target="_blank" rel="noopener noreferrer" href={event.link}>Event Link</a>
+                        </div>
                     </div>
                 )}
+                <button className="detailsButton" onClick={this.handleChange}>{`${buttonText} Details`}</button>
 
             </div>
         );
