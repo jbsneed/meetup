d1ee57926b3dd43de1a2619df63bab6593fae752
Julie <juliebell14@gmail.com>
2020-02-10 15:42:40 -0600

    add time

--- src/Event.js
+++ src/Event.js
@@ -42,7 +42,7 @@ class Event extends Component {
         return (
             <div className="Event">
                 <div className="eventName">{event.name}</div>
-                <div className="eventDate">{event.local_date} at {event.local_time}</div>
+                <div className="eventDate">{event.local_date && event.local_time}</div>
                 {this.state.showDetails && (
                     <div className="eventDetails">
                         {event.rsvp_limit &&
